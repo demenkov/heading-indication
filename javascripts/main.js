41f504e868475cb6c89e121e7068606bea0be825
Ivan Demenkov <ivan@demenkov.com>
2012-10-30 00:56:35 +0300

    Added some new shit

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('This would be the main JS file.');
+//main function
+jQuery(document).ready(function($){
+	$("#datepicker").datepicker({
+		/*minDate: "-30",
+		maxDate: "+1m +1w +3d"*/
+		dateFormat:	"dd.mm.yy",
+		defaultDate: null,
+	});
+});
