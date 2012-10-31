587ee6a7d67272831a95000c5e3edac543f44351
Ivan Demenkov <ivan@demenkov.com>
2012-11-01 01:05:39 +0300

    changed datepicker, added inputs for longitude and latitude

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@
 jQuery(document).ready(function($) {
 	"use strict";
 	//add date interactivity
-	$.fn.datepicker.DPGlobal.dates.daysMin = ["Сб", "По", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+	/*$.fn.datepicker.DPGlobal.dates.daysMin = ["Сб", "По", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
 	$.fn.datepicker.DPGlobal.dates.months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
-	$.fn.datepicker.DPGlobal.dates.monthsShort = ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"];
+	$.fn.datepicker.DPGlobal.dates.monthsShort = ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"];*/
 	$('#inputDate').datepicker({
 		format : 'dd.mm.yyyy',
-		weekStart : 1
+		weekStart : 1,
+		language : 'ru'
 	})
 	.on('changeDate', function() {
 		$('#inputDate').datepicker('hide');
@@ -20,11 +21,12 @@ jQuery(document).ready(function($) {
 	})
 	.siblings('span.add-on')
 	.on('click', function(e) {
-		$('#inputDate').datepicker('show');
+		/*$('#inputDate').datepicker('show');
 		e.stopPropagation();
 		$('body').on('click', function() {
 			$('#inputDate').datepicker('hide');
-		});
+		});*/
+		$('#inputDate').focus();
 	});
 	//add time interactivity
 	$('#inputTime').timepicker({
