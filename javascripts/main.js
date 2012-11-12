e1ee33afaafa73682d6b928d028f63c4456f8386
Ivan Demenkov <ivan@demenkov.com>
2012-11-12 19:49:30 +0300

    Remove old files

--- javascripts/main.js
+++
@@ -1,9 +0,0 @@
-//main function
-jQuery(document).ready(function($){
-	$("#datepicker").datepicker({
-		/*minDate: "-30",
-		maxDate: "+1m +1w +3d"*/
-		dateFormat:	"dd.mm.yy",
-		defaultDate: null,
-	});
-});
