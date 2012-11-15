dcbde10246989c852e07ce631fa772066344b11d
Ivan Demenkov <ivan@demenkov.com>
2012-11-16 01:41:24 +0300

    Calculate from sun

--- js/main.js
+++ js/main.js
@@ -6,6 +6,13 @@ jQuery(document).ready(function($) {
 		return number * Math.PI / 180;
 	}
 
+	//enable content tabs
+	$('#mainMenu a').click(function (e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
+	$('#mainMenu a:first').tab('show');
+
 	//add date interactivity
 	$('#inputDate').datepicker({
 		format		: 'dd.mm.yyyy',
@@ -14,9 +21,11 @@ jQuery(document).ready(function($) {
 	})
 	.on('changeDate', function() {
 		$('#inputDate').datepicker('hide');
+		calculate();
 	})
 	.on('blur', function() {
 		$('#inputDate').datepicker('hide');
+		calculate();
 	})
 	.on('focus', function(e) {
 		e.stopPropagation();
@@ -66,11 +75,32 @@ jQuery(document).ready(function($) {
 		$(this).val(val);
 	});
 
-	$('#timeZone').on('keyup', calculate);
+	$('#timeZone')
+	.add('#inputTime')
+	.add('#inputLatitude')
+	.add('#inputLatitudeSec')
+	.add('#inputLongitude')
+	.add('#inputLongitudeSec')
+	.add('#cp')
+	.on('keyup', calculate);
+
+	$('.btn').on('click', calculate);
+
+	$('#inputTime').on('change', calculate);
 
 	function calculate() {
+
+		$('#inputSunAzimuth')
+		.add('#inputSunRa')
+		.add('#inputSunGradient')
+		.add('#inputSunLha')
+		.val('');
+
 		var d = $('#inputDate').val().split('.'),
 			t = $('#inputTime').val().split(':'),
+			longitude = $('#inputLongitude').val(),
+			longitudeSec = $('#inputLatitudeSec').val(),
+			west = $('.eathWest button.west').hasClass('active') ? true : false,
 			date = {
 				day			: parseInt(d[0]),
 				month		: parseInt(d[1]),
@@ -83,22 +113,61 @@ jQuery(document).ready(function($) {
 				minutes	: parseInt(t[1]),
 				seconds	: parseInt(t[2])
 			},
-			UT = time.hours + time.minutes/60 + time.seconds/3600,
+			UT = time.hours + time.minutes/60 + time.seconds/3600,//(12.3)
 			JD = 1720996.5 - 
 			Math.round(date.yearStroke/100) + 
 			Math.round(date.yearStroke/400) + 
 			Math.round(365.25 * date.yearStroke) + 
 			Math.round(30.6 * (date.monthStroke + 1)) + 
 			date.day + 
-			UT/24,
-			tau = date.day/36525,
-			//M01 = 357.528 + 35999.05 * tau + 0.04107 * UT,//средняя аномалия солнца
-			M0 = 357.52910 + 35999.05030 * tau - 0.0001559 * 2 * tau - 0.00000048 * 3 * tau, //средняя аномалия солнца
-			L0 = 280.46 + 36000.772 * tau + 0.04107 * UT; //средняя долгота солнца
+			UT/24,//(2.20)
+			tau			= date.day/36525, //(12.2)
+			M0Sun		= 359.9286,
+			K0			= 1.9154,
+			E0			= 23.4412,
+			Omega0Moon	= 55.199,
+			L0			= 279.6034;
 
-		M0 = (M0 > 360) ? 360 : M0;
-		var L = L0 + (1.915 - 0.0048 * tau) * Math.sin(Math.rad(M0)) + 0.02 * Math.sin(Math.rad(2 * M0));//долгота Солнца
-		L = (L > 360) ? L-360 : L;
+		var RSun			= 0.26696 + 0.00447 * Math.cos(M0Sun), //(12.9)
+			deltaMStroke	= 0.985647 * UT / 24, //(12.18)
+			deltaM			= 35999.05 * tau, //(12.17)
+			OmegaMoon		= Omega0Moon - 1934.14 * tau; // (12.8)
+
+		var MSun 	= M0Sun + deltaM + deltaMStroke, //(12.16)
+			K		= K0 - 0.004881 * tau; //(12.15)
+
+		var nu				= K * Math.sin(MSun) + 0.02 * Math.sin(2 * MSun), //(12.14)
+			deltaPsi		= -0.00479 * Math.sin(OmegaMoon), //(12.9)
+			deltaL			= 36000 * tau + 0.768925 * tau, //(12.2)
+			deltaLstroke	= 0.985647 * UT / 24; //(12.13)
+
+		var L = L0 + deltaL + deltaLstroke; //(12.11)
+		var lambdaSun = L + nu + deltaPsi, //(12.9)
+			deltaE = 0.002666 * Math.cos(OmegaMoon); //(12.7)
+		var E = E0 - 0.013012 * tau + deltaE; //(12.6)
+		var betaSun = Math.asin(Math.sin(lambdaSun) * Math.sin(E)); // (12.5)
+		var alphaStrokeSun = Math.acos(Math.cos(lambdaSun) / Math.cos(betaSun)); //(12.4)
+		var alphaSun = (betaSun > 0) ? alphaStrokeSun : 360 - alphaStrokeSun;
+
+		var fullLongitude = longitude + longitudeSec / 60;
+
+		if (fullLongitude) {
+			$('#inputSunLha').val(fullLongitude);
+		}
+		
+		if (betaSun) {
+			$('#inputSunGradient').val(betaSun);
+		}
+
+		if (alphaSun) {
+			$('#inputSunRa').val(alphaSun);
+		}
+
+
+			/*23.4412
+			55.199
+			279.6034
+			99.6056*/
 	}
 
 	//change buttons state at the same time 
@@ -143,4 +212,5 @@ jQuery(document).ready(function($) {
 	}
 
 	//$('#inputLatitude').add('#inputLongitude').on('change', calculate);
+	calculate();
 });
