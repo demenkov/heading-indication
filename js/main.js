ceaccc0b88fb1223dd2b4c4e61d23c1ee7765650
Ivan Demenkov <ivan@demenkov.com>
2012-11-28 01:07:52 +0300

    try prao

--- js/main.js
+++ js/main.js
@@ -160,37 +160,252 @@ jQuery(document).ready(function($) {
 		return (367*y)-Math.floor((7/4)*(Math.floor((m+9)/12)+y))+Math.floor(275*m/9)+d-730531.5+(u/24)
 	}
 
-	function JulianDay(yy,mm,dd) {
-		var tulos;
-		tulos =367*yy-Math.floor(7*(yy+Math.floor((mm+9)/12))/4)
-		-Math.floor(3*(Math.floor((yy+(mm-9)/7)/100)+1)/4)
-		+Math.floor(275*mm/9)+dd+1721028.5;
-		return tulos;
-	}
-	function Greenwich (yy,mm,dd,time) {
-		// calculates Greenwich sidereal time in hours
-		var T,JD,GWT0,GW,GMST;
-		JD = JulianDay(yy,mm,dd);
-		// JD is Julian Day
-		T = (JD - 2451545.0)/36525;
-		// GWT0 is Greenwich Sidereal Time in hours
-		GWT0 = (24110.54841+8640184.812866*T+0.093104*T*T-0.0000062*T*T*T)/3600;
-		if (GWT0 >= 24)
-		GW = GWT0 - 24*Math.floor(GWT0/24);
-		if (GWT0 <= - 24)
-		GW = 24 -(Math.abs(GWT0) - 24*Math.floor(Math.abs(GWT0)/24));
-		GMST = GW + 1.00273790935*time;
-		if (GMST >= 24)
-		GMST = GMST - 24;
-		if (GMST < 0 )
-		GMST = GMST + 24;
-		return GMST;
-	}
+	function J_day_(Day,Month,Year) {
+		var i,d;
+		d = 0;
 
-	function Deg(as,min,sek) {
-		// converts degree,min,sec to degree
-		return (as + min/60 + sek/3600);
-	}
+		for (i=1995; i<=Year-1; i++)
+		{
+		if  ( ( i/4-Math.floor(i/4) ) == 0 )
+		{d = parseFloat(d) + 366}
+		else
+		{d = parseFloat(d) + 365};
+		};
+
+		for (i=1; i<=Month-1; i++)
+		{
+		if ((i==1) || (i==3) || (i==5) || (i==7) || (i==8) || (i==10) || (i==12))
+		{d = Math.floor(d) + 31};
+		if ( (i==4) || (i==6) || (i==9) || (i==11) )
+		{d = Math.floor(d) + 30};
+		if ( (i==2) )
+		{if  ((i/4-Math.floor(i/4)) == 0 )
+		{d = Math.floor(d) + 29}
+		else
+		{d = Math.floor(d) + 28};
+		};
+		};
+		//var J_day = parseFloat(d) + parseFloat(Day) + parseFloat(2449717.5);
+		var J_day = Math.floor(d) + Day + 2449717.5;
+		return J_day;
+	};
+
+	//{**********************************************************}
+	function L_star_jd(j_d)
+	//{       определяется долгота солнца юлианскому дню         }
+	//{              cредняя ошибка - 12"                        }
+	//{Const
+	{
+		var  e = 0.01671;
+		var  r =1296000;
+		var  l,m,lam,t2000;
+		t2000 = (j_d - 2451545)/36525;
+		l = parseFloat(1009678) + (100*r + 2771)*t2000;    //{ средняя долгота }
+		// document.AZ_H.test.value="t2000="+t2000+ " j_d="+ j_d+" l="+l;
+		l = l*2*Math.PI/1296000;
+		m = parseFloat(1287100) + parseFloat((99*r + 1292581)*t2000);
+		//  document.AZ_H.test.value="m="+m+ " l="+l;
+		//{ средняя аномалия }
+		m = m*2*Math.PI/1296000;
+		lam = l + 2*e*Math.sin(m) + 1.25*e*e*Math.sin(2*m);// { истинная долгота }
+
+		//document.AZ_H.test.value="lam="+lam+ " m="+ m+" l="+l;
+		return lam; //J_day;
+	};
+
+	function StarTime(iday,im,iyear) {
+		//---Const
+		//--- r : real = 1296000.0;    //                 {    ! 360.*3600.}
+
+		var r = 1296000.0;
+
+		var month = new Array(31,28,31,30,31,30,31,31,30,31,30,31);
+
+
+		var i;
+		//integer
+		var t,sm,p,e,q,d,f,m,l,pl,ps,s0,ihs,ims,sec;
+		// : real;
+		//{*--------------------------------------------------------------------*
+		//*                        Calculate iday and t                        *
+		//*--------------------------------------------------------------------*}
+
+		if (im != 1) {
+		//--	i = (iyear div 4)*4;
+		var tmp_=Math.floor(iyear/4);
+		i=Math.round(4*tmp_);
+		if (iyear == i)
+		    {month[2] = 29};
+		//[1] - v JS s 0 numeracija massiva!, no v dannom sluchae - my uzhe pribavili izvne funkcii
+		for (i=1; i<=im-1; i++)  {iday += month[i-1]};
+		};
+
+		//50-ja stroka
+
+		var iy = iyear - 1900;
+		//--- iday = (iday-1)+(iy-1) div 4;
+		iday = Math.floor((iday-1)+(iy-1)/4);
+
+		//document.loc_star.test.value=iy+','+iday+"; "+l_hour+":"+l_min+":"+l_sec;
+
+
+		//--t := Longint(iday) + Longint(iy)*365.0;
+		t=iday + iy*365.0;
+		t = (t+0.5)/36525.0;   // {   ! 00.01.1900 12h UT}
+		t = t - 1;    // {   ! 01.01.2000 12h UT1}
+		//60-ja stroka
+		//{*--------------------------------------------------------------------*
+		//*                    Calculate mean sidereal time                    *
+		//*--------------------------------------------------------------------*}
+		sm = 24110.548410 + 8640184.8128660*t + 0.093104*t*t- 0.00000620*t*t*t;
+		while (sm <= 0) {sm = sm + 86400.0};
+		while (sm > 86400) {sm = sm - 86400.0};
+		//{*--------------------------------------------------------------------*
+		//*             Calculate long and short periodic nutation             *
+		//*--------------------------------------------------------------------*}
+		//70-ja stroka
+		p = Math.PI/180.0/3600.0;
+		//{-------------------}
+		e = p*(84381.448 - 46.8150*t - 0.00059*t*t + 0.0018130*t*t*t);
+		//{-------------------}
+		q = p*( 450160.280 -   5.0*r*t - 482890.539*t+ 7.455*t*t + 0.0080*t*t*t);
+		d = p*(1072261.3070 + 1236.0*r*t + 1105601.328*t - 6.891*t*t+ 0.0190*t*t*t);
+		f = p*( 335778.8770 + 1342.0*r*t + 295263.1370*t - 13.2570*t*t+ 0.0110*t*t*t);
+		m = p*(1287099.804 +  99.0*r*t+1292581.2240*t -  0.5770*t*t - 0.0120*t*t*t);
+		l = p*( 485866.7330+1325.0*r*t + 715922.633*t + 31.3100*t*t+ 0.0640*t*t*t);
+		//80-ja stroka{*-------------------}
+
+		pl =  -(17.19960 + 0.017420*t)*Math.sin(q);
+
+		//pl=sin(q);
+		pl = pl + (0.20620 + 0.000020*t)*Math.sin(2.0*q);
+		pl = pl +   0.00460            *Math.sin(q+2.0*f-2.0*l);
+		pl = pl +   0.00110            *Math.sin(2.0*(l-f));
+		pl = pl -   0.00030            *Math.sin(2.0*(q+f-l));
+		pl = pl-   0.00030            * Math.sin (l-m-d);
+		pl = pl-   0.00020            * Math.sin (q-2.0*d+2.0*f-2.0*m);
+		pl = pl+   0.00010            * Math.sin (q-2.0*f+2.0*l);
+		pl = pl-( 1.31870+ 0.000160*t)* Math.sin (2.0*(q-d+f));
+		pl = pl+(  0.14260-0.000340*t)* Math.sin (m);
+		pl = pl-(  0.05170-0.000120*t)* Math.sin (2.0*q-2.0*d+2.0*f+m);
+		pl = pl+(  0.02170-0.000050*t)* Math.sin (2.0*q-2.0*d+2.0*f-m);
+		pl = pl+(  0.01290+0.000010*t)* Math.sin (q-2.0*d+2.0*f);
+		pl = pl+   0.00480            * Math.sin (2.0*(l-d));
+		pl = pl-   0.00220            * Math.sin (2.0*(f-d));
+		pl = pl+(  0.00170-0.000010*t)* Math.sin (2.0*m);
+		pl = pl-   0.00150            * Math.sin (q+m);
+		pl = pl-(  0.00160-0.000010*t)* Math.sin (2.0*(q-d+f+m));
+		pl = pl-   0.00120            * Math.sin (q-m);
+		pl = pl-   0.00060            * Math.sin (q+2.0*d-2.0*l);
+		pl = pl-   0.00050            * Math.sin (q-2.0*d+2.0*f-m);
+		pl = pl+   0.00040            * Math.sin (q-2.0*d+2.0*l);
+		pl = pl+   0.00040            * Math.sin (q-2.0*d+2.0*f+m);
+		pl = pl-   0.00040            * Math.sin (l-d);
+		pl = pl+   0.00010            * Math.sin (2.0*l+m-2.0*d);
+		pl = pl+   0.00010            * Math.sin (q+2.0*d-2.0*f);
+		pl = pl-   0.00010            * Math.sin (2.0*d-2.0*f+m);
+		pl = pl+   0.00010            * Math.sin (2.0*q+m);
+		pl = pl+   0.00010            * Math.sin (q+d-l);
+		pl = pl-   0.00010            * Math.sin (m+2.0*f-2.0*d);
+		//111-ja stroka{*------------------- }
+		ps =   -(  0.22740+0.000020*t)* Math.sin (2.0*(q+f));
+		ps = ps+(  0.07120+0.000010*t)* Math.sin (l);
+		ps = ps-(  0.03860+0.000040*t)* Math.sin (q+2.0*f);
+		ps = ps-   0.03010            * Math.sin (2.0*q+2.0*f+l);
+		ps = ps-   0.01580            * Math.sin (l-2.0*d);
+		ps = ps+   0.01230            * Math.sin (2.0*q+2.0*f-l);
+		ps = ps+   0.00630            * Math.sin (2.0*d);
+		ps = ps+(  0.00630+0.000010*t)* Math.sin (q+l);
+		ps = ps-(  0.00580+0.000010*t)* Math.sin (q-l);
+		ps = ps-   0.00590            * Math.sin (2.0*q+2.0*d+2.0*f-l);
+		ps = ps-   0.00510            * Math.sin (q+2.0*f+l);
+		ps = ps-   0.00380            * Math.sin (2.0*(q+d+f));
+		ps = ps+   0.00290            * Math.sin (2.0*l);
+		ps = ps+   0.00290            * Math.sin (2.0*q-2.0*d+2.0*f+l);
+		ps = ps-   0.00310            * Math.sin (2.0*(q+f+l));
+		ps = ps+   0.00260            * Math.sin (2.0*f);
+		ps = ps+   0.00210            * Math.sin (q+2.0*f-l);
+		ps = ps+   0.00160            * Math.sin (q+2.0*d-l);
+		ps = ps-   0.00130            * Math.sin (q-2.0*d+l);
+		ps = ps-   0.00100            * Math.sin (q+2.0*d+2.0*f-l);
+		ps = ps-   0.00070            * Math.sin (l+m-2.0*d);
+		ps = ps+   0.00070            * Math.sin (2.0*q+2.0*f+m);
+		ps = ps-   0.00070            * Math.sin (2.0*q+2.0*f-m);
+		ps = ps-   0.00080            * Math.sin (2.0*q+2.0*d+2.0*f+l);
+		ps = ps+   0.00060            * Math.sin (2.0*d+l);
+		ps = ps+   0.00060            * Math.sin (2.0*(q-d+f+l));
+		ps = ps-   0.00060            * Math.sin (q+2.0*d);
+		ps = ps-   0.00070            * Math.sin (q+2.0*d+2.0*f);
+		ps = ps+   0.00060            * Math.sin (q-2.0*d+2.0*f+l);
+		ps = ps-   0.00050            * Math.sin (q-2.0*d);
+		ps = ps+   0.00050            * Math.sin (l-m);
+		ps = ps-   0.00050            * Math.sin (q+2.0*f+2.0*l);
+		ps = ps-   0.00040            * Math.sin (m-2.0*d);
+		ps = ps+   0.00040            * Math.sin (l-2.0*f);
+		ps = ps-   0.00040            * Math.sin (d);
+		ps = ps-   0.00030            * Math.sin (l+m);
+		ps = ps+   0.00030            * Math.sin (l+2.0*f);
+		ps = ps-   0.00030            * Math.sin (2.0*q+2.0*f-m+l);
+		ps = ps-   0.00030            * Math.sin (2.0*q+2.0*d+2.0*f-m-l);
+		ps = ps-   0.00020            * Math.sin (q-2.0*l);
+		ps = ps-   0.00030            * Math.sin (2.0*q+2.0*f+3.0*l);
+		ps = ps-   0.00030            * Math.sin (2.0*q+2.0*d+2.0*f-m);
+		ps = ps+   0.00020            * Math.sin (2.0*q+2.0*f+m+l);
+		ps = ps-   0.00020            * Math.sin (q-2.0*d+2.0*f-l);
+		ps = ps+   0.00020            * Math.sin (q+2.0*l);
+		ps = ps-   0.00020            * Math.sin (2.0*q+l);
+		ps = ps+   0.00020            * Math.sin (3.0*l);
+		ps = ps+   0.00020            * Math.sin (2.0*q+d+2.0*f);
+		ps = ps+   0.00010            * Math.sin (2.0*q-l);
+		ps = ps-   0.00010            * Math.sin (l-4.0*d);
+		ps = ps+   0.00010            * Math.sin (2.0*(q+d+f-l));
+		ps = ps-   0.00020            * Math.sin (2.0*q+4.0*d+2.0*f-l);
+		ps = ps-   0.00010            * Math.sin (2.0*l-4.0*d);
+		ps = ps+   0.00010            * Math.sin (2.0*q-2.0*d+2.0*f+m+l);
+		ps = ps-   0.00010            * Math.sin (q+2.0*d+2.0*f+l);
+		ps = ps-   0.00010            * Math.sin (2.0*q+4.0*d+2.0*f-2.0*l);
+		ps = ps+   0.00010            * Math.sin (2.0*q+4.0*f-l);
+		ps = ps+   0.00010            * Math.sin (l-m-2.0*d);
+		ps = ps+   0.00010            * Math.sin (q-2.0*d+2.0*f+2.0*l);
+		ps = ps-   0.00010            * Math.sin (2.0*(q+d+f+l));
+		ps = ps-   0.00010            * Math.sin (q+2.0*d+l);
+		ps = ps+   0.00010            * Math.sin (2.0*q-2.0*d+4.0*f);
+		ps = ps+   0.00010            * Math.sin (2.0*q-2.0*d+2.0*f+3.0*l);
+		ps = ps-   0.00010            * Math.sin (l+2.0*f-2.0*d);
+		ps = ps+   0.00010            * Math.sin (q+2.0*f+m);
+		ps = ps+   0.00010            * Math.sin (q+2.0*d-m-l);
+		ps = ps-   0.00010            * Math.sin (q-2.0*f);
+		ps = ps-   0.00010            * Math.sin (2.0*q-d+2.0*f);
+		ps = ps-   0.00010            * Math.sin (2.0*d+m);
+		ps = ps-   0.00010            * Math.sin (l-2.0*f-2.0*d);
+		ps = ps-   0.00010            * Math.sin (q+2.0*f-m);
+		ps = ps-   0.00010            * Math.sin (q-2.0*d+m+l);
+		ps = ps-   0.00010            * Math.sin (l-2.0*f+2.0*d);
+		ps = ps+   0.00010            * Math.sin (2.0*(l+d));
+		ps = ps-   0.00010            * Math.sin (2.0*q+4.0*d+2.0*f);
+		ps = ps+   0.00010            * Math.sin (d+m);
+
+		var s0 = sm+(pl+ps)/15.0* Math.cos (e);
+		//{*--------------------------------------------------------------------*
+		//*                            Print results                           *
+		//*--------------------------------------------------------------------*}
+
+		var star_time = Math.PI/12*s0/3600.0;
+
+		//document.AZ_H.test.value="s0="+s0+" star_time_rad="+star_time;
+
+		return star_time;
+		//в радианах
+	};
+
+	function MS(s) {
+	//Begin
+	while (s < 0) {s = s + 24};
+	while (s > 24) {s = s - 24};
+	s = s*(Math.PI/12);
+	return s;
+	//End;
+	};
 
 	//http://wildphoto.irk.ru/travel/sun.html
 	//http://planetcalc.ru/320/?language_select=ru
@@ -236,81 +451,140 @@ jQuery(document).ready(function($) {
 	}
 
 	Math.star = function(lg,la,ye,mo,da,ho,mi,se,zo,alpha,beta) {
-		var uu=ut(ho,mi,se,zo);
-		var jj=jd(ye,mo,da,uu);
-		var T=jj/36525;
+		var pi= Math.PI;
+		var p0 = pi/12;
+		var e1 = 23;  //{ наклон эклиптики }
+		var e2 = 26;
+		var e3 = 23;
 
-		var omega0 = 55.199;
-		var omega = omega0 - 1934.14*T;
-		var deltaPsi = -0.00479 * Math.sin(omega);
-		var deltaEps = 0.00266 * Math.cos(omega);
-		var eps0 = 23.4412;
-		var eps = eps0 - 0.013012 * T + deltaEps;
+		var longitude_h = lg/15;
 
+		var e = pi*(e1 + e2/60 + e3/3600)/180;
+		var e4 = Math.cos(e);
+		var e5 = Math.sin(e);
+		var p1 = Math.cos(pi/6);
+		var p2 = Math.sin(pi/6);
 
-		var deltaAlphaPR = (1.2808 + 0.5566 * Math.tan(beta) * Math.sin(alpha))*T;
-		var deltaBetaPR = 0.5566 * Math.cos(alpha) * T;
+		var t =  ye - 1950 + (mo - 0.5)/12;
+		var t0 = t/100 + 0.5;
+		var a5 = pi*(1.5 + t0*0.00532578);
+		var a6 = Math.cos(a5);
+		var a7 = Math.sin(a5);
 
-		var deltaAlphaN = deltaPsi * Math.cos(eps) + Math.sin(eps) * Math.sin(alpha) * Math.tan(beta) - deltaEps * Math.tan(beta) * Math.cos(alpha);
-		var deltaBetaN = deltaPsi * Math.sin(eps) * Math.cos(alpha) + deltaEps * Math.sin(alpha);
+		var e0 = 0.0167510 - (0.0000418 + 0.000000126*t0)*t0;
+		//  { g - долгота солнца в перигелии }
+		var g = 4.90816 + pi*(15 + (6189.03 + (1.63 + 0.012*t0)*t0)*t0)/648000;
+		var g1 = Math.cos(g);
+		var g2 = Math.sin(g);
 
-		var L0 = 279.6034;
-		var deltaL = 36000 * T + 0.768925 * T;
-		var L = L0 + deltaL;
+		var v1 = -16;   //{ лучевая скорость }
+		var Year = 1996; //{ год }
 
-		var deltaAlphaA = -0.0057 * Math.sec(beta) * (Math.sin(L) * Math.cos(eps) + Math.cos(L) * Math.cos(alpha) * Math.cos(eps));
-		var deltaBetaA = -0.0057 * Math.sin(L) * Math.cos(alpha) * Math.sin(beta) + Math.cos(L) * ( Math.sin(eps) * Math.cos(beta) + Math.cos(eps) * Math.cos(alpha) * Math.sin(beta));
+		var a4 = Math.rad(alpha);
+		var d4 = Math.rad(beta);
 
-		var S0 = 99.6056;
+		var j_d = J_day_(da,mo,ye);
 
-		var tgr = S0 + deltaL + deltaPsi * Math.cos(eps);
+		var  a = a4 + t*p0*(3.07327 + 1.33617*Math.sin(a4)*Math.sin(d4)/Math.cos(d4))/3600;
+		var  d = d4 + t*pi*Math.cos(a4)*3.09299e-5;
+		var  d5 = Math.cos(d);
+		var  d6 = Math.sin(d);
 
-		tgr = (tgr > 360) ? (tgr/360 - Math.round(tgr/360)) * 360 - 180 : tgr;
-		tgr = (tgr < 360) ? tgr + 360 : tgr;
+		var  c1 = d5*Math.cos(a);
+		var  c2 = d5*Math.sin(a);
+		var  c4 = c2*e4 + d6*e5;
 
-		var ra = alpha + deltaAlphaA + deltaAlphaN + deltaAlphaPR;
-		var dec = beta + deltaBetaA + deltaBetaN + deltaBetaPR;
+		var sin_Latitude=Math.sin(la/180*pi);
+		var cos_Latitude=Math.cos(la/180*pi);
 
-		var tgrs = tgr + 360.98565 * ((ho-zo+mi/60+se/3600)/24) - alpha;
-		var tm = tgrs + lg;
+		var  d7 = sin_Latitude//0.816641
+			*d6;
+		var  d8 = cos_Latitude//0.577146
+			*d5;
+		var  d9 = cos_Latitude//0.577146
+			*d6;
+		var  d0 = sin_Latitude//0.816641
+			*d5;
+		//  { v2 поправка за движение солнца к апексу }
+		var  v2 = -19.5*((c1*a6 + c2*a7)*p1 + p2*d6);
+		//  { поправка за эксцентриситет орбиты земли }
+		var  h = e0*(c1*g2 - c4*g1);
+		var  y = (L_star_jd(j_d + 1) - L_star_jd(j_d))/24;
 
-		//console.log(tgrs)
+		var s0 = StarTime(da,mo,ye);
 
-		/*var k=Math.PI/180.0;
-		var deltaAlphaPR = (1.2808 + 0.5566 * Math.tan(beta) * Math.sin(alpha))*T;
-		var deltaBetaPR = 0.5566 * Math.cos(alpha) * T;
-		var M=357.5291+35999.0503*T-0.0001559*T*T-0.00000045*T*T*T;
-		M=M % 360
-		M = k*M;
-		var Lo=280.46645+36000.76983*T+0.0003032*T*T;
-		Lo=Lo % 360;
-		var DL=(1.9146-0.004817*T-0.000014*T*T)*Math.sin(M)+(0.019993-0.000101*T)*Math.sin(2*M)+0.00029*Math.sin(3*M);
-		var L=Lo+DL;
-		var omega = 125.04 - 1934.136*T;
-		var eps=23.43999-0.013*T;
-		var deltaEps = 0.00266*Math.cos(omega);
-		var deltaPsi = -0.00479*Math.sin(omega);
-		var deltaAlphaN = deltaPsi * Math.cos(eps) + Math.sin(eps) * Math.sin(alpha) * Math.tan(beta) - deltaEps * Math.tan(beta) * Math.cos(alpha);
-		var deltaBetaN = deltaPsi * Math.sin(eps) * Math.cos(alpha) + deltaEps * Math.sin(alpha);
-		var deltaAlphaA = -0.0057 * Math.sec(beta) * (Math.sin(L) * Math.cos(eps) + Math.cos(L) * Math.cos(alpha) * Math.cos(eps));
-		var deltaBetaA = -0.0057 * Math.sin(L) * Math.cos(alpha) * Math.sin(beta) + Math.cos(L) * ( Math.sin(eps) * Math.cos(beta) + Math.cos(eps) * Math.cos(alpha) * Math.sin(beta));
-		var alphaStar = alpha + deltaAlphaA + deltaAlphaN + deltaAlphaPR;
-		var betaStar = beta + deltaBetaA + deltaBetaN + deltaBetaPR;
+		var ir = s0*12/pi;
+		var	gr = Math.floor(ir);
+		var	min_ =  Math.floor((ir-gr)*60);
+		var	sec =(ir-gr-min_/60)*3600;
 
-		var GMST=280.46061837+360.98564736629*jj+0.000387933*T*T-T*T*T/38710000;
-		GMST=(GMST+360) % 360;
-		var S0 = GMST;
-		var tgr = S0+DL+deltaPsi*Math.cos(eps);
-		tgr = (tgr+360)%360;
-		var tgrs = tgr-alphaStar+360.98565*((ho+mi/60+se/3600)/24)
+		var t_=Math.floor(ho)+mi/60+se/3600;
+		t_=t_+zo;
 
-		var azimuth = Math.deg(Math.atan(Math.cos(Math.rad(lg)) * Math.tan(Math.rad(alphaStar)) * Math.cosec(Math.rad(tgrs)) - Math.sin(Math.rad(lg)) * Math.cot(Math.rad(tgrs))));*/
+		s0 = parseInt(gr) + min_/60 + sec/3600 + longitude_h;
+		//2.50556 - у Рудницкого!
+		var s_00=s0-longitude_h;
+
+		var s=s0 //в часах - на местную полночь
+		+(zo)*1.002737909350795;
+
+		var s_=parseFloat(s)+(parseFloat(t_))*1.002737909350795;
+
+		s=MS(s);
+		//s- в радианах!
+		var  ak0 = (a - s)*0.997271/p0;
+		  //{ ak0 - момент верхней кульминации по моск. времени }
+		while (ak0 < 0) {ak0 = ak0 + 24 - 0.0656667};
+		while (ak0 > 24) {ak0 = ak0 - 24};
+
+		var  k1 = Math.floor(ak0);
+		var  k2 = Math.floor((ak0-k1)*60); //frac(ak0)*60;
+		var  k3 = Math.floor((ak0-k1-k2/60)*3600);
+
+		var  r1 = k1;
+		var  r2 = k2; //exit;
+
+		var  u = t_;
+		//var l__=L_star_jd(j_d);
+		var  z = L_star_jd(j_d) + parseFloat(y*u);
+		var  q = Math.cos(z);
+		var  r = Math.sin(z);
+		//document.AZ_H.test.value="u="+u+ " z="+ z+" y="+y+ " r="+ r;
+		s = s0 + u + 9.85*u/3600;
+		//текущее время захватили, ранее в s было время полночи!
+		s= MS(s);
+
+		//var  t9 = s - a;
+		var  t9 = s - a;
+		//текущее
+		//зв. время в радианах минус альфа - часовой угол!
+
+		var  b5 = Math.cos(t9);
+		var  b6 = Math.sin(t9);
+		var  b9 = d7 + d8*b5;
+		var  b0 = Math.sqrt(1 - b9*b9);
+		var  b3 = d5*b6/b0;
+		var  b4 = (d0*b5 - d9)/b0;
+		var H_all=180*Math.atan(b9/b0)/pi;
+		var  b8 = Math.floor(180*Math.atan(b9/b0)/pi + 0.5);
+		var  b7 = 180*Math.atan(b3/b4)/pi;
+		if (b4 > 0)
+		 {
+		 if (b7 < 0)
+		  {b7 = b7 + 360}
+		 }
+		else
+		 {b7 = b7 + 180};
+
+		if (b7>180) {b7=b7-360};
+		var azimut=Math.abs(b7);
+
 
 		return {
-			/*ha		: tgrs,
-			ra		: alphaStar,
-			dec		: betaStar,
-			azimuth	: azimuth*/
+			ha		: H_all,
+			ra		: alpha,
+			dec		: beta,
+			azimuth	: azimut
 		};
 	}
 
